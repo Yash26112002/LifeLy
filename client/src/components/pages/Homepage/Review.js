f287baeb18afedfbfc32815f092a40c6308bdff2
Akash Rajoria <akashrajoria250103@gmail.com>
2023-05-10 13:44:27 +0530

    Add real mongodb reviews

--- client/src/components/pages/Homepage/Review.js
+++ client/src/components/pages/Homepage/Review.js
@@ -1,47 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SingleReview from './SingleReview'
-import './review.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import SingleReview from './SingleReview';
+import './review.css';
+import { SERVER_URL } from '../../../config';
+
 const Review = () => {
-  const searchItem = [
-    {
-      id: 0,
-      name: 'Vijay',
-      star: '2',
-      content:
-        ' There is currently very little understanding of the influence of website quality on user behaviour. Future quality assessments might use the criteria informed by key behaviour change theories.'
-    },
-    {
-      id: 1,
-      name: 'Vijay',
-      star: '2',
-      content:
-        ' There is currently very little understanding of the influence of website quality on user behaviour. Future quality assessments might use the criteria informed by key behaviour change theories.'
-    },
-    {
-      id: 2,
-      name: 'Vijay',
-      star: '2',
-      content:
-        ' There is currently very little understanding of the influence of website quality on user behaviour. Future quality assessments might use the criteria informed by key behaviour change theories.'
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    async function fetchReviews() {
+      const response = await axios.get(`${SERVER_URL}/review/get`);
+      setReviews(response.data);
     }
-  ]
+    fetchReviews();
+  }, []);
+
   return (
     <div id='review' className='review'>
-      <div className='circle'></div><div className='circle2'></div>
+      <div className='circle'></div>
+      <div className='circle2'></div>
       <div style={{ paddingTop: '4%' }} className='problem-div-que'>
         What people say about <span className='logo'>LifeLy</span>
       </div>
       <div className='review-card-outer'>
-        {searchItem.map(ex => {
-          return (
-            <div className='review-card-outer-div'>
-              <SingleReview key={ex.id} {...ex} />
-            </div>
-          )
-        })}
+        {reviews.map((review) => (
+          <div className='review-card-outer-div'>
+            <SingleReview key={review._id} review={review} />
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
-export default Review
+  );
+};
+
+export default Review;
--- client/src/components/pages/Homepage/SingleReview.js
+++ client/src/components/pages/Homepage/SingleReview.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import './singlereview.css'
-const SingleReview = props => {
+import React from 'react';
+import './singlereview.css';
+
+const SingleReview = ({ review }) => {
   return (
     <div className='review-card'>
       <div className='review-card-inner'>
-        <div className='review-card-name'>{props.name}</div>
-        <div>{props.star} stars</div>
-        <div className='review-card-text'>{props.content}</div>
+        <div className='review-card-name'>{review.name}</div>
+        <div>{review.rating} stars</div>
+        <div className='review-card-text'>{review.review}</div>
       </div>
     </div>
-  )
-}
-export default SingleReview
+  );
+};
+
+export default SingleReview;
